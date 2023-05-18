9c9897722b7c442fb597720c1196d9c4cfe9ac90
dilanasanga <dilanasanga.x@gmail.com>
2023-05-17 17:50:26 -0700

    generating random numbers with Copilot

--- random-number.py
+++ random-number.py
@@ -1,4 +1,9 @@
 import random
+#generate function to generate random number
+def generate():
+    return random.randint(1,100)
+#call the function
+print (generate())
+#rand_num = random.randint(1,100)
+#print (rand_num)
 
-rand_num = random.randint(1,100)
-print (rand_num)
