63cfac0750b5e26f110ddbe8c407b073f9839caf
dilanasanga <dilanasanga.x@gmail.com>
2024-02-29 15:04:15 -0800

    running code and added comments

---
+++ average-function.py
@@ -0,0 +1,18 @@
+#Define the function
+def number_average(inputnumbers):
+#Define the total variable    
+    total = 0
+#Loop through the list
+    for i in inputnumbers:
+#Add the values in list to the total(convereted the string in list to an int)
+        total = total + int(i)
+#fidn the average
+    average = total/int(len(inputnumbers))
+    print(average)
+
+#Input values
+input_string = input()
+#Input is split by spaces and
+inputnumbers = input_string.split()
+#Call function
+number_average(inputnumbers)
