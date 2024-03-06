ff8a8a0de5c191767b4fda98fdc8008ec19cc2ed
dilanasanga <dilanasanga.x@gmail.com>
2024-03-05 22:53:43 -0800

    correcting calculations and adding input methods

--- numberreverse.py
+++ numberreverse.py
@@ -2,8 +2,6 @@
 #If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 #Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
-intmax = 2**31-1
-intmin = -2**31
 class Solution(object):
     def reverse(self, x):
         xoriginal = x
@@ -11,19 +9,19 @@ class Solution(object):
         finalstring = ''
         y = []
         for i in range(len(str(x1))):
-#            out = x1%10
-#            x1 = x1//10
-            out, x1 = divmod(x1, 10)
+            out = x1%10
+            x1 = x1//10
+#            out, x1 = divmod(x1, 10)
             y.append(out)
-            print(y,xoriginal)
         finalstring = ''.join(map(str,y))
-	finalint = int(finalstring)
-	if {finalint < intmax || finalint > intmin}:
-        	if x < 0:
-            		return(0-finalint)
-        	else:
-            		return(finalint)
-	else:
-		return(0)
-instance1 = (Solution(int(43222)))
-instance1.Solution
+        finalint = int(finalstring)
+        if finalint < 2**31-1 and finalint > -2**31:
+            if x < 0:
+                return(0-finalint)
+            else:
+                return(finalint)
+        else:
+            return(0)
+b = int(input("Insert Number: "))
+a = Solution()
+print(a.reverse(b))
